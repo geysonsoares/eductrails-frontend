219558e9b4b47cf86af6dc1a92317e631104b127
Geyson Soares <geyson.soares@outlook.com>
2019-09-01 11:51:51 -0300

    Adiciona no ao fluxo do caminho do estudante apos a etapa de avaliação.

--- src/app/pages/forms/employer-form/employer-form.component.ts
+++ src/app/pages/forms/employer-form/employer-form.component.ts
@@ -10,6 +10,20 @@ export class EmployerFormComponent{
   @HostBinding('class.employer-form') private readonly employerForm = true;
   public qualifications = ['Young Padawan', 'Junior', 'Middle', 'Senior'];
 
+  questionarioRespondido:boolean;
+
   constructor(private router: Router) {
+     if(localStorage.getItem('questionarioRespondido') === 'true'){
+      this.questionarioRespondido = true;
+     }else{
+      this.questionarioRespondido = false;
+     }
+
+     localStorage.removeItem('questionarioRespondido');
   } 
+
+  deveExibir(): boolean{
+    console.log(this.questionarioRespondido);
+    return this.questionarioRespondido;
+  }
 }
